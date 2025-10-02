a2fc98d60314f4ae6c4e037abfc4ef226397d1f6
Rodrigo Aranda <rodrigo.aranda.developer@gmail.com>
2025-10-02 00:16:42 -0300

    error build

--- components/time-line.tsx
+++ components/time-line.tsx
@@ -9,7 +9,7 @@ const TimeLine = () => {
         {/* Línea central */}
         <div className="absolute top-8 left-0 w-full h-[2px] bg-slate-300"></div>
 
-        {dataAboutPage.map((data, index) => (
+        {dataAboutPage.map((data) => (
           <div key={data.id} className="relative flex flex-col items-center min-w-[200px]">
             {/* Puntos en la línea */}
             <div className="relative z-10 w-6 h-6 flex items-center justify-center rounded-full bg-indigo-600 border-4 border-slate-50 shadow-lg"></div>
